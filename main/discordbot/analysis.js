51cd4e4af01fe533a12fd04d722b5b5d7ea1f8af
안형빈 <nks0005@naver.com>
2022-06-09 10:35:16 +0900

    에러 발생

--- main/discordbot/analysis.js
+++ main/discordbot/analysis.js
@@ -1,3 +1,5 @@
+const https = require('https'); // https 사이트 크롤링을 위한 모듈
+
 class Mutex {
     constructor() {
         this.lock = false;
@@ -36,7 +38,33 @@ class analysis {
         this.updateAll();
     }
 
+    async getJsonbyUrl(url) {
+        return new Promise((resolve, reject) => {
+            const request = https.request(url, function(res) {
+                let data = ``;
 
+                res.on('data', function(chunk) { data += chunk.toString(); });
+                res.on('end', function() {
+                    if (res.statusCode === 200) {
+                        console.log(`${url} OK`);
+                        const body = JSON.parse(data);
+                        resolve(body);
+                        // 처리 함수
+                    } else {
+                        reject(`${url}에 대한 OK 에러 발생`)
+                    }
+                });
+            });
+            request.on('error', function(err) {
+                reject(`${url}에 대한 request 에러 발생`);
+            });
+            request.setTimeout(50000, function() {
+                reject(`${url} timeout`);
+            });
+
+            request.end();
+        });
+    }
 
     updateAll() {
         // 최근 킬보드 20개를 불러와 조건에 맞는 킬보드를 배열에 담습니다.
@@ -46,10 +74,68 @@ class analysis {
         }
     }
 
+    process_event(body) {
+        let test = new Object();
+        test.test2 = body['EventId'];
+
+        return test;
+    }
+
+    async process_battle(json) { // 배틀 로그를 가공하는 작업
+        return new Promise((resolve, reject) => {
+            const battlelog = json[0];
+
+            let battleItem = new Object();
+            battleItem.battleId = battlelog['id']; // 배틀 아이디
+            battleItem.totalKills = battlelog['totalKills']; // 총 킬 수
+            battleItem.totalPlayers = Object.keys(battlelog['players']).length; // 유저 수
+            battleItem.startTime = new Date(battlelog['startTime']); // 시작 시간
+            battleItem.killevent = new Array(); // 킬 이벤트를 담을 배열
+
+            for (let i = 0; i < battleItem.totalKills; i++) {
+                // 총 킬 수 만큼의 이벤트 값을 얻는다
+                try {
+                    (async(arg) => {
+                        const start = arg;
+                        let body = await this.getJsonbyUrl(`https://gameinfo.albiononline.com/api/gameinfo/events/battle/${battleItem.battleId}?offset=${start}&limit=1`);
+
+                        let event = this.process_event(body);
+                        battleItem.killevent.push(event);
+                        resolve(battleItem);
+                    })(i);
+                } catch (e) {
+                    reject(e);
+                }
+            }
+
+            // console.log(battleItem);
+        });
+    }
+
     async update(url) {
-        // url에 접속하여 데이터를 얻는다.
+        let complete = false;
+        while (true) {
+            // url에 접속하여 데이터를 얻는다.
 
 
+            try {
+                const body = await this.getJsonbyUrl(url);
+                let battleItem = await this.process_battle(body);
+
+                // lock
+                this.mutex.use();
+                this.arrTotal.push(battleItem);
+                // unlock
+                this.mutex.release();
+                console.log(this.arrTotal.killevent);
+
+                complete = true;
+            } catch (e) {
+                console.log(e);
+            }
+
+            if (complete === true) break;
+        }
     }
 }
 async function auto_analysis() {
@@ -77,4 +163,9 @@ function testjson() {
 
 }
 
-testjson();
+function testAnalysis() {
+    let a = new analysis();
+    a.updateAll();
+}
+
+testAnalysis();
