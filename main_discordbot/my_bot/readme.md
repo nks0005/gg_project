7863f0b4d512db8853299cdfd9b86f0244b85412
안형빈 <nks0005@naver.com>
2022-06-09 22:05:18 +0900

    update_killboard.js
    논리가 꼬여버린것으로 보임

---
+++ main_discordbot/my_bot/readme.md
@@ -0,0 +1,70 @@
+# 데이터베이스 구조
+> 간단하고 최소로 필요한 부분으로 구성
+
+## TABLE battlelog
+battleid | totalkills | totalplayers | starttime
+
+## TABLE killevent
+eventid | battleid | timestamp | groupmembercount | numberofparticipants | killarea
+
+## TABLE player
+id | version[0lastkiller, 1killer, 2victim] | avgip | eq_id | name | guildname | allianceName | killfame | deathfame | damagedone | supporthealingdone
+
+killer = killFame, DamageDone, SupportHealingDone
+victim = DeathFame
+
+
+## TABLE equipment
+id | mainhand | offhand | head | armor | shoes | cape | potion | food
+
+
+
+
+# SQL
+```sql
+create database my_bot;
+use my_bot;
+
+CREATE TABLE IF NOT EXISTS battlelog (
+    battleid INT PRIMARY KEY NOT NULL,
+    totalkills INT NOT NULL,
+    totalplayers INT NOT NULL,
+    starttime TIMESTAMP NOT NULL,
+    endtime TIMESTAMP NOT NULL
+) ENGINE=InnoDB;
+
+CREATE TABLE IF NOT EXISTS killevent (
+    eventid INT PRIMARY KEY NOT NULL,
+    battleid INT NOT NULL,
+    starttime TIMESTAMP NOT NULL,
+    partymembercount INT NOT NULL,
+    killercount INT NOT NULL,
+    killarea VARCHAR(255) NOT NULL
+) ENGINE=InnoDB;
+
+CREATE TABLE IF NOT EXISTS player(
+    playerid VARCHAR(255) PRIMARY KEY NOT NULL ,
+    whois INT NOT NULL,
+    avgip INT NOT NULL,
+    eqid INT NOT NULL,
+    playername VARCHAR(255) NOT NULL,
+    guildname VARCHAR(255),
+    alliancename VARCHAR(255),
+    killfame INT,
+    deathfame INT,
+    damage INT,
+    heal INT
+) ENGINE=InnoDB;
+
+CREATE TABLE IF NOT EXISTS equiment(
+    eqid INT AUTO_INCREMENT PRIMARY KEY,
+    mainhand VARCHAR(255),
+    offhand VARCHAR(255),
+    head VARCHAR(255),
+    armor VARCHAR(255),
+    shoes VARCHAR(255),
+    cape VARCHAR(255),
+    potion VARCHAR(255),
+    food VARCHAR(255)
+) ENGINE=InnoDB;
+```
---
+++ main_discordbot/my_bot/update_killboard.js
@@ -0,0 +1,222 @@
+// 데이터베이스에 주기적으로 킬보드를 업로드
+const https = require('https');
+const mysql2 = require('mysql2');
+
+
+class battlelog {
+
+    constructor() {
+        this.dbcon = mysql2.createConnection({
+            host: "127.0.0.1",
+            user: "root",
+            password: "root",
+            database: 'my_bot'
+        });
+
+        this.battleMax = 20;
+
+        this.urlBattlelog = `https://gameinfo.albiononline.com/api/gameinfo/battles?offset=0&limit=${this.battleMax}&sort=recent`;
+    }
+
+    async getJsonbyUrl(url) {
+        let complete = false;
+        let body = ``;
+        while (true) {
+            try {
+                body = await this.getJsonbyUrl2(url);
+                complete = true;
+            } catch (e) {
+                //console.log(e);
+            }
+            if (complete === true) return body;
+        }
+    }
+    async getJsonbyUrl2(url) {
+        return new Promise((resolve, reject) => {
+            const request = https.request(url, function(res) {
+                let data = ``;
+
+                res.on('data', function(chunk) { data += chunk.toString(); });
+                res.on('end', function() {
+                    if (res.statusCode === 200) {
+                        console.log(`${url} OK`);
+                        const body = JSON.parse(data);
+                        resolve(body);
+                        // 처리 함수
+                    } else {
+                        reject(`${url}에 대한 OK 에러 발생`)
+                    }
+                });
+            });
+            request.setTimeout(50000, function() {
+                reject(`${url} timeout`);
+            });
+            request.on('error', function(err) {
+                reject(`${url}에 대한 request 에러 발생`);
+            });
+
+
+            request.end();
+        });
+    }
+
+    check_equip(arg) {
+        if (arg == null) {
+            arg = {};
+            arg['Type'] = '0';
+        }
+        return arg;
+    }
+
+    // eqid 값을 얻기 위해서는 db에 올린 후 값을 받아야한다.
+    async process_equip(equip) {
+        let eq = new Object();
+        eq.mainhand = this.check_equip(equip['MainHand'])['Type'];
+        eq.offhand = this.check_equip(equip['OffHand'])['Type'];
+        eq.head = this.check_equip(equip['Head'])['Type'];
+        eq.armor = this.check_equip(equip['Armor'])['Type'];
+        eq.shoes = this.check_equip(equip['Shoes'])['Type'];
+        eq.cape = this.check_equip(equip['Cape'])['Type'];
+        eq.potion = this.check_equip(equip['Potion'])['Type'];
+        eq.food = this.check_equip(equip['Food'])['Type'];
+
+        const [row] = await this.dbcon.promise().query(`INSERT IGNORE INTO equiment (mainHand, offHand, head, armor, shoes, cape, potion, food) VALUES ('${eq.mainhand}', '${eq.offhand}', '${eq.head}', '${eq.armor}', '${eq.shoes}', '${eq.cape}', '${eq.potion}' ,'${eq.food}');`);
+        return parseInt(row.insertId);
+    }
+
+    async process_player(kill, whois) {
+        let killer = new Object();
+        killer.playerid = kill['Id'];
+        killer.whois = whois;
+        killer.avgip = parseInt(kill['AverageItemPower']);
+        killer.eqid = await this.process_equip(kill['Equipment']);
+        killer.playername = kill['Name'];
+        killer.guildname = kill['GuildName'];
+        killer.alliancename = kill['AllianceName'];
+
+        if (whois === 0) { // 막타 유저
+            killer.killfame = kill['KillFame'];
+        } else
+        if (whois === 1) { // victim. 죽은자
+            killer.deathfame = kill['DeathFame'];
+        } else if (whois === 2) { // Participants. 
+            killer.damage = kill['DamageDone'];
+            killer.heal = kill['SupportHealingDone'];
+        }
+
+        return killer;
+    }
+
+    async processall_db(battles, killevents, players) {
+        for (var i = 0; i < battles.length; i++) {
+            const battle = battles[i];
+            await this.dbcon.promise().query(`INSERT IGNORE INTO battlelog (battleid, totalkills, totalplayers, starttime, endtime) VALUES
+            ('${battle.battleid}', '${battle.totalkills}', '${battle.totalplayers}', '${battle.starttime}', '${battle.endtime}');`);
+        }
+
+        for (var i = 0; i < killevents.length; i++) {
+            const killevent = killevents[i];
+            console.log(killevent.starttime);
+            await this.dbcon.promise().query(`INSERT IGNORE INTO killevent (eventid, battleid, starttime, partymembercount, killercount, killarea) VALUES ('${killevent.eventid}', '${killevent.battleid}', '${killevent.starttime}', '${killevent.partymembercount}', '${killevent.killercount}', '${killevent.killarea}');`);
+        }
+
+        for (var i = 0; i < players.length; i++) {
+            const player = players[i];
+
+            var sql = ``;
+            if (parseInt(player.whois) === 0) { // 막타 killfame
+                sql = `
+                INSERT IGNORE INTO player (playerid, whois, avgip, eqid, playername, guildname, alliancename, killfame)
+                VALUES 
+                ('${player.playerid}', '${player.whois}', '${player.avgip}', '${player.eqid}', '${player.playername}', '${player.guildname}', '${player.alliancename}', '${player.killfame}');`;
+            } else if (parseInt(player.whois) === 1) { // victim deathfame
+                sql = `
+                INSERT IGNORE INTO player (playerid, whois, avgip, eqid, playername, guildname, alliancename, deathfame)
+                VALUES 
+                ('${player.playerid}', '${player.whois}', '${player.avgip}', '${player.eqid}', '${player.playername}', '${player.guildname}', '${player.alliancename}', '${player.deathfame}');`;
+            } else if (parseInt(player.whois) === 2) { // part, damage heal
+                sql = `
+                INSERT IGNORE INTO player (playerid, whois, avgip, eqid, playername, guildname, alliancename, damage, heal)
+                VALUES 
+                ('${player.playerid}', '${player.whois}', '${player.avgip}', '${player.eqid}', '${player.playername}', '${player.guildname}', '${player.alliancename}', '${player.damage}', '${player.heal}');`;
+            }
+            await this.dbcon.promise().query(sql);
+        }
+
+
+
+    }
+
+    async update() {
+        try {
+            let body_bb = await this.getJsonbyUrl(this.urlBattlelog); // body[0] ~ body[this.battleMax-1]
+
+            // 이벤트 로그
+            const killevents = new Array();
+            const players = new Array();
+            const battles = new Array();
+
+            for (var i = 0; i < this.battleMax; i++) {
+                const battleboard = body_bb[i];
+
+                let battlelogs = new Object();
+                // TABLE battlelog
+                battlelogs.battleid = parseInt(battleboard['id']);
+                battlelogs.totalkills = parseInt(battleboard['totalKills']);
+                battlelogs.totalplayers = Object.keys(battleboard['players']).length;
+                battlelogs.starttime = new Date(battleboard['startTime']);
+                battlelogs.endtime = new Date(battleboard['endTime']);
+                battles.push(battlelogs);
+
+
+                const urlEventlog = `https://gameinfo.albiononline.com/api/gameinfo/events/battle/${battlelogs.battleid}?offset=0&limit=${battlelogs.totalkills}`;
+                let body_ev = await this.getJsonbyUrl(urlEventlog);
+
+                const totaleventcount = battlelogs.totalkills;
+                for (var j = 0; j < totaleventcount; j++) {
+                    const eventboard = body_ev[j];
+
+                    if (parseInt(eventboard['Victim']['AverageItemPower']) === 0) { // 평균 템렙이 0인 경우 = 버그 킬
+                        battlelogs.totalkills = battlelogs.totalKills - 1;
+                        continue;
+                    }
+
+                    // killevent
+                    var killevent = new Object();
+                    killevent.eventid = parseInt(eventboard['EventId']);
+                    killevent.battleid = parseInt(eventboard['BattleId']);
+                    killevent.timestamp = new Date(eventboard['TimeStamp']);
+                    killevent.partymembercount = parseInt(eventboard['groupMemberCount']);
+                    killevent.killercount = Object.keys(eventboard['Participants']).length;
+                    killevent.killarea = eventboard['KillArea'];
+                    killevents.push(killevent);
+
+                    // 킬을 먹은 유저 정보 (막타)
+                    const lastkiller = eventboard['Killer'];
+                    players.push(this.process_player(lastkiller, 0));
+
+                    // Participants로 부터 killer들의 정보를 추출
+                    const totalkillercount = killevent.killercount;
+                    for (var k = 0; k < totalkillercount; k++) {
+                        const partiboard = eventboard['Participants'][k];
+                        players.push(this.process_player(partiboard, 2));
+                    }
+
+                    // victim 정보 추출
+                    const victim = eventboard['Victim'];
+                    players.push(this.process_player(victim, 1));
+                }
+            }
+
+            // db 입력
+            await this.processall_db(battles, killevents, players)
+
+        } catch (err) {
+            console.log(err);
+        }
+        return true;
+    }
+}
+
+let bat = new battlelog();
+bat.update();
