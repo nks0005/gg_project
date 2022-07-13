c0d192a176590625cd886357030ca098c2cb94fa
안형빈 <nks0005@naver.com>
2022-07-13 19:38:44 +0900

    Version 1.1.9
    헬게 구함 테스트

--- main/discordBot/buttonTest.js
+++ main/discordBot/buttonTest.js
@@ -3,6 +3,13 @@ const { MessageActionRow, MessageButton, Client } = require("discord.js");
 const { guildId, hellgate55ChannelId, statisticsChannelId, usersearchChannelId, botToken } = require('./config/config.json'); // 설정 값
 const wait = require('node:timers/promises').setTimeout;
 
+const Monitor = require('./modules/monitor');
+const Hellgate = require('./modules/hellgate');
+const Statistics = require('./modules/statistics');
+const SearchUser = require('./modules/searchUser');
+const TotalStat = require('./modules/totalstat');
+const TotalList = require('./modules/totallist');
+
 
 const client = new Client({
     intents: ["GUILDS", "GUILD_MESSAGES"]
@@ -10,14 +17,6 @@ const client = new Client({
 
 client.on('ready', () => {
     console.log(`Logged ${client.user.tag}`);
-});
-
-client.on('message', async message => {
-    if (message.content.startsWith('!reply')) {
-        message.lineReply('Hey'); //Line (Inline) Reply with mention
-
-        message.lineReplyNoMention(`My name is ${client.user.username}`); //Line (Inline) Reply without mention
-    }
 });
 
 
@@ -37,25 +36,37 @@ client.on('interactionCreate', async interaction => {
                 .setCustomId('match')
                 .setLabel('5v5 조합 통계')
                 .setStyle('PRIMARY')
+            )
+            .addComponents(
+                new MessageButton()
+                .setCustomId('findparty')
+                .setLabel('5v5 파티 구해요')
+                .setStyle('SUCCESS')
             );
 
-        await interaction.reply({ content: '버튼 테스트!', components: [row] });
+        await interaction.reply({ content: '원하는 기능을 선택하세요. [파티 구해요] 기능을 사용하려면 디스코드 닉네임을 인게임 아이디로 만들어야합니다.\n예시) (길드마크)닉네임 or 닉네임 => 🎴wanthealcome or wanthealcome', components: [row] });
 
     }
 
-    const filter = i => i.customId === 'total' || i.customId === 'match';
-    const collector = interaction.channel.createMessageComponentCollector({ filter, time: 15000 });
+    const filter = i => i.customId === 'total' || i.customId === 'match' || i.customId === 'findparty';
+    const collector = interaction.channel.createMessageComponentCollector({ filter });
 
     collector.on('collect', async i => {
         if (i.customId === 'total') {
-            //await i.deferUpdate();
-            await i.reply({ content: '5v5 판수 통계 출력 <#994850279822463087>', components: [], ephemeral: true });
+            await i.reply({ content: '5v5 판수 통계 출력 <#994850279822463087>', ephemeral: true });
         } else if (i.customId === 'match') {
-            await i.reply({ content: '5v5 조합 통계 출력 <#994850279822463087>', components: [], ephemeral: true });
+            await i.reply({ content: '5v5 조합 통계 출력 <#994850279822463087>', ephemeral: true });
+        } else if (i.customId === 'findparty') {
+            await i.reply({ content: '<#956318538937827348>채널에 구인 광고를 올렸습니다.', ephemeral: true });
+
+            const searchUser = new SearchUser.modules(client.guilds.cache.get(guildId).channels.cache.get("956318538937827348"));
+            const userName = interaction.member.displayName.replace(/([\uE000-\uF8FF]|\uD83C[\uDF00-\uDFFF]|\uD83D[\uDC00-\uDDFF])/g, '');
+            await searchUser.update(userName);
+
+            await client.guilds.cache.get(guildId).channels.cache.get("956318538937827348").send({ content: `${userName}이 5v5 레드헬게 파티를 구하고 있습니다!` });
         }
     });
 
-    collector.on('end', collected => console.log(`Collected ${collected.size} items`));
 
 });
 client.login(botToken);
